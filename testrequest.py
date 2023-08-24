2e1cbb4d367a389c6a432904667d7314dde59d34
kwehen <kwesihenry11@gmail.com>
2023-08-23 23:28:23 -0400

    edited testrequest.py
    additional strings for different current conditions and extracting + printing temp in the final string

--- testrequest.py
+++ testrequest.py
@@ -1,18 +1,29 @@
 import requests
+from requests import get
 import json
 import sensitive
 
+
 base_url = "https://api.weatherapi.com/v1/current.json"
 api_key = sensitive.api_key
+# ip = get('https://api.ipify.org').content.decode('utf8')
 zip_code = input("Enter your zip code: ")
 
 r = requests.get(f"{base_url}?key={api_key}&q={zip_code}")
 r = r.json()
 condition = r["current"]["condition"]["text"]
+temperature = r["current"]["temp_f"]
 
 string = "cloudy"
+string2 = "clear"
+string3 = "rain"
 
 if string in condition:
-    print("Right not it is cloudy")
-else:
-    print("Right now it is not cloudy")
+    beginning = "Right now it is cloudy"
+elif string2 in condition:
+    beginning = "Right now it is clear"
+elif string3 in condition:
+    beginning = "Right now it is raining"
+
+
+print(f"{beginning} and the temperature is {temperature} degrees Fahrenheit")
