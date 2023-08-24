d294851d1d8e6b208fee1557d70c0e4a25a7a50d
kwehen <kwesihenry11@gmail.com>
2023-08-23 23:28:23 -0400

    edited testrequest.py
    created an infinite loop so if the user inputs an invalid zip code the program will start again

--- testrequest.py
+++ testrequest.py
@@ -4,26 +4,30 @@ import json
 import sensitive
 
 
-base_url = "https://api.weatherapi.com/v1/current.json"
-api_key = sensitive.api_key
-# ip = get('https://api.ipify.org').content.decode('utf8')
-zip_code = input("Enter your zip code: ")
+while True:
+    base_url = "https://api.weatherapi.com/v1/current.json"
+    api_key = sensitive.api_key
+    # ip = get('https://api.ipify.org').content.decode('utf8')
+    zip_code = input("Enter your zip code: ")
 
-r = requests.get(f"{base_url}?key={api_key}&q={zip_code}")
-r = r.json()
-condition = r["current"]["condition"]["text"]
-temperature = r["current"]["temp_f"]
+    r = requests.get(f"{base_url}?key={api_key}&q={zip_code}")
+    r = r.json()
+    if "error" in r:
+        print(f"{zip_code} is not a valid zip code")
+    else:
+        condition = r["current"]["condition"]["text"]
+        temperature = r["current"]["temp_f"]
 
-string = "cloudy"
-string2 = "clear"
-string3 = "rain"
+        string = "cloudy"
+        string2 = "clear"
+        string3 = "rain"
 
-if string in condition:
-    beginning = "Right now it is cloudy"
-elif string2 in condition:
-    beginning = "Right now it is clear"
-elif string3 in condition:
-    beginning = "Right now it is raining"
+        if string in condition:
+            beginning = "Right now it is cloudy"
+        elif string2 in condition:
+            beginning = "Right now it is clear"
+        elif string3 in condition:
+            beginning = "Right now it is raining"
 
-
-print(f"{beginning} and the temperature is {temperature} degrees Fahrenheit")
+        print(f"{beginning} and the temperature is {temperature} degrees Fahrenheit")
+        break
