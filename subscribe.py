34feb3749d6119473cf519917db7472063b4c766
kwehen <kwesihenry11@gmail.com>
2023-08-25 15:47:28 -0400

    created subscribe.py
    forgot to hit save lol

--- subscribe.py
+++ subscribe.py
@@ -8,23 +8,6 @@ logger = logging.getLogger()
 sns_client=boto3.client('sns')
 email = input("Enter your email: ")
 
-# def subscribe(topic, protocol, endpoint):
-#     try:
-#         subscription = sns_client.subscribe(
-#         TopicArn=sensitive.sns_topic_arn, Protocol=protocol, Endpoint=endpoint, ReturnSubscriptionArn=True)
-#         logger.info("Subscribed %s %s to topic %s.", protocol, endpoint, topic)
-#         print("Please confirm subscribtion...")
-#     except ClientError:
-#         logger.exception(
-#             "Couldn't subscribe %s %s to topic %s.", protocol, endpoint, topic)
-#         print("Couldn't subscribe!")
-#         raise
-    
-#     else:
-#         return subscription
-
-# subscribe(sensitive.sns_topic_arn, 'email', email)
-
 def create_topic(name):
     try:
         topic = sns_client.create_topic(Name=name)
