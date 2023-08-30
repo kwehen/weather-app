42ef36aa2d0c0f485417003b930694d899e964b1
kwehen <kwesihenry11@gmail.com>
2023-08-29 20:23:08 -0400

    edited request2.py
    changed flow and updated for better flow of the application

--- request2.py
+++ request2.py
@@ -50,52 +50,20 @@ def subscribe(topic, protocol, endpoint):
 
 topicArn = create_topic(topic_name)
 subscribe(topicArn, 'email', email)
+
 while True:
-    base_url = "https://api.weatherapi.com/v1/current.json"
-    api_key = sensitive.api_key
-    # email = input("Enter your email: ")
-    # zip_code = input("Enter your zip code: ")
-    # sns_client = boto3.client('sns')
-    # logger = logging.getLogger()
-
-    # def create_topic(name):
-    #     try:
-    #         topic = sns_client.create_topic(Name=name)
-    #         logger.info("Created topic %s with ARN %s.", name, topic)
-    #         print(f"{name} created.")
-    #     except ClientError:
-    #         logger.exception("Couldn't create topic %s.", name)
-    #         print(f"Could not create {name}.")
-    #         raise
-    #     else:
-    #         return topic
-    
-    # topicArn = create_topic(topic_name)
-
-    # def subscribe(topic, protocol, endpoint):
-    #     try:
-    #         subscription = sns_client.subscribe(
-    #         TopicArn=topic['TopicArn'], Protocol=protocol, Endpoint=endpoint, ReturnSubscriptionArn=True)
-    #         logger.info("Subscribed %s %s to topic %s.", protocol, endpoint, topic)
-    #         print("Please confirm subscribtion...")
-    #     except ClientError:
-    #         logger.exception(
-    #             "Couldn't subscribe %s %s to topic %s.", protocol, endpoint, topic)
-    #         print("Couldn't subscribe!")
-    #         raise
-
-    #     else:
-    #         return subscription
-
-    # subscribe(topicArn, 'email', email)
-
     print("Has the subscription been confirmed? y/n")
     if input() == "y":
         print("Subscribed!")
-        pass
+        break
     elif input() == "n":
         print("Please confirm subscribtion...")
         continue
+
+while True:
+    base_url = "https://api.weatherapi.com/v1/current.json"
+    api_key = sensitive.api_key
+
     r = requests.get(f"{base_url}?key={api_key}&q={zip_code}")
     r = r.json()
     if "error" in r:
@@ -118,6 +86,6 @@ while True:
         elif string4 in condition:
             beginning = f"Right now in {area} it is overcast"
 
-        response = sns_client.publish(TopicArn=topicArn,Message=f"{beginning} and the temperature is {temperature} degrees Fahrenheit.")
+        response = sns_client.publish(TopicArn=topicArn['TopicArn'],Message=f"{beginning} and the temperature is {temperature} degrees Fahrenheit.")
         print("Message sent")
         break
