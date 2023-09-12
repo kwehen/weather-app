56593466ca3b5d77a67d10c0500d1a335d31c004
kwehen <kwesihenry11@gmail.com>
2023-09-12 06:48:33 -0400

    edited test-gmail.py
    added carrier dictionary for to phone number. Also added try/except for errors. Added some more stuff too

--- test-gmail.py
+++ test-gmail.py
@@ -2,10 +2,20 @@ import smtplib, ssl
 import sensitive
 from email.mime.text import MIMEText
 
+carrier_dict = {
+    "AT&T": "txt.att.net",
+    "T-Mobile": "tmomail.net",
+    "Verizon": "vtext.com",
+}
+
+number = input("Enter your phone number: ")
+carrier = input("Enter your carrier: ")
+
+if carrier in carrier_dict:
+    reciever_email = f"{number}@{carrier_dict[carrier]}"
 port = 465
 password = sensitive.email_password
 sender_email = sensitive.sender_email
-reciever_email = sensitive.reciever_email
 subject = "Weather Updates"
 text = "test"
 context = ssl.create_default_context()
@@ -13,5 +23,9 @@ message = f'Subject: {subject}\n\n This is a test email'.format(subject, text)
 
 
 with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
-    server.login(sender_email, password)
-    server.sendmail(sender_email, reciever_email, message)
+    try:
+        server.login(sender_email, password)
+        server.sendmail(sender_email, reciever_email, message)
+        print("Message sent!")
+    except:
+        print("Something went wrong")
